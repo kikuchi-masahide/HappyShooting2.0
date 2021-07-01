ac154d1efdc745ac7eeae3fb94978af719de2e59
ano0406 <m20000406k@gmail.com>
2021-07-01 12:15:58 +0900

    ・小数(double)と行列を送るためのクラスを作った(未テスト)

---
+++ DX12DecimalToShader.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "DX12IShaderTransferable.h"
+
+/// <summary>
+/// シェーダに小数を渡すためのクラス
+/// double型小数をfloatとして渡す
+/// </summary>
+class DX12DecimalToShader final
+	:public DX12IShaderTransferable
+{
+public:
+	DX12DecimalToShader()
+	{}
+	~DX12DecimalToShader()
+	{}
+	void Map(void* map_pointer)
+	{
+		float* float_pointer = static_cast<float*>(map_pointer);
+		*float_pointer = static_cast<float>(data_);
+	}
+	SIZE_T GetSize()
+	{
+		return sizeof(float);
+	}
+	//実データ
+	double data_;
+};
